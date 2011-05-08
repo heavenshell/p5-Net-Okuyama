4729b88496ced809cd42f153de5c4ffa33c4ee97
s_ohyanagi <heavenshell.jp@gmail.com>
2011-05-08 17:22:27 +0900

    Add get_keys_by_tag, remove, close sample.

--- examples/sample.pl
+++ examples/sample.pl
@@ -1,19 +1,30 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use Perl6::Say;
 use Net::Okuyama;
 
 # Connect to okuyama automatically.
-my $client = Net::Okuyama->new('localhost:8888', 'localhost:8889');
+my $client = Net::Okuyama->new(hosts => ['localhost:8888', 'localhost:8889']);
 
 # Set record.
 $client->set(foo => 'bar');
-warn $client->get('foo'); # bar
+say $client->get('foo'); # bar
 
 # Get record.
 $client->set('foo', 'baz');
-warn $client->get('foo'); # baz
+say $client->get('foo'); # baz
 
 # Set tags.
 $client->set(foo => 'bar', ['baz']);
-warn $client->get('foo'); # bar
+say $client->get('foo'); # bar
+
+$client->set(bar => 'baz', ['baz']);
+my $keys = $client->get_keys_by_tag('baz');
+for my $key (@{$keys}) {
+    say $client->get($key);
+}
+
+$client->remove('foo');
+$client->remove('bar');
+$client->close;
